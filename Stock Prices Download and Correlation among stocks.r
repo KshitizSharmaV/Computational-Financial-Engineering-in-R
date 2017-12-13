f95144929c3a4381de2ec82030ac7f977214773c
Kshitiz <kshitizsharmav@gmail.com>
2017-12-13 11:36:27 -0500

    Stock data download and correlation among stocks
    Functions this file will help you with
    
    1) Download Equity data 
    2) Calculate daily and monthly returns
    3) Find Correlation among stocks
    4) Find two highest correlated pair of stocks.
    
    Please twerk variables according to your requirement. 
    Disclaimer : I don't suggest you to use the code for real life analysis. This code is for educational purpose only.

---
+++ Stock Prices Download and Correlation among stocks.r
@@ -0,0 +1,70 @@
+library(quantmod)
+library(tseries)
+library(chron)
+library(timeDate)
+
+# Intialize your tickers
+ticker <- c("JPM", "WFC", "BAC", "C", "GS", "V", "AIG", "MS", "CME", "DFS")
+
+# tabe for stroring log returns
+daily.log.return <- NULL
+
+#Download data and store log returns in the table
+for(i in 1:10){
+  table1 <- getSymbols(ticker[i], from = "2014-01-01", auto.assign = F)
+  temp.log.return <- dailyReturn(table1, type = "log")
+  daily.log.return <- cbind(daily.log.return, temp.log.return)
+}
+
+colnames(daily.log.return) <- ticker
+
+monthly.simple.return <- NULL
+
+# Calculate monthly reyturns for same tickers
+for(i in 1:10){
+  table1 <- getSymbols(ticker[i], from = "2014-01-01", auto.assign = F)
+  temp.simple.return <- monthlyReturn(table1, type = "arithmetic")
+  monthly.simple.return <- cbind(monthly.simple.return, temp.simple.return)
+  
+}
+
+monthly.simple.return <- as.data.frame(monthly.simple.return)
+
+# Normality test for each equity
+jarque.bera.test(monthly.simple.return[,1])
+# Since the P-value is 0.5656, this indicates this equity is normal distributed
+jarque.bera.test(monthly.simple.return[,2])
+jarque.bera.test(monthly.simple.return[,3])
+jarque.bera.test(monthly.simple.return[,4])
+jarque.bera.test(monthly.simple.return[,5])
+jarque.bera.test(monthly.simple.return[,6])
+jarque.bera.test(monthly.simple.return[,7])
+jarque.bera.test(monthly.simple.return[,8])
+jarque.bera.test(monthly.simple.return[,9])
+jarque.bera.test(monthly.simple.return[,10])
+
+# Correlaion between each stock
+cor.table <- cor(daily.log.return)
+
+for(i in 1:10){
+  cor.table[i,i] <- 0
+}
+
+# equity having higest correlation
+which.max(cor.table)
+#equity BAC and C has the highest correlation
+
+
+timeline1 <- getSymbols("BAC", from = "2014-01-01", auto.assign = F)
+timeline1 <- as.data.frame(timeline1)
+
+write.csv(timeline1, row.names=T, "temp.csv")
+
+timeline2 <- read.csv("temp.csv")
+
+timeline3 <- as.Date(timeline2$X)
+nrow(daily.log.return)
+
+# Graphs for 2 highest correlated stocks
+plot(timeline3, daily.log.return[,3], type = "l")
+lines(timeline3, daily.log.return[,4], type = "l", col = "red")
